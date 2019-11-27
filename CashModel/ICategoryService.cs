7e0f8f3c8b3eae555e0e9d1c829ed2c7f194f043
oussama idbid <oussamaidbid@gmail.com>
2019-11-28 00:59:00 +0100

    daaa

---
+++ CashModel/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCash.CashModel
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> GetCategories();
+        //Task<bool> CreateProduct(Product product);  
+        //Task<bool> EditProduct(int id, Product product);  
+
+        //Task<IEnumerable<Product>> GetProductsByCategory(int categoryId);
+        //Task<Product> SingleProduct(int id);  
+        //Task<bool> DeleteProduct(int id);  
+    }
+}
